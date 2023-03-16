83823977484d0585c5b395d36fde13d94f5ee5d3
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-16 22:13:18 +0600

    added

--- src/components/list/RelatedVideoList.js
+++ src/components/list/RelatedVideoList.js
@@ -1,6 +1,6 @@
 import RelatedVideoListItem from "./RelatedVideoListItem";
 
-export default function RelatedVideoList() {
+export default function RelatedVideoList({ currentVideoId, tags }) {
     return (
         <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
             <RelatedVideoListItem />
